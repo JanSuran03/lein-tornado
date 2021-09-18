6fa9e24a972b547daa33d12c237cf3246c3b733d
JanSuran <suran.orgpad@gmail.com>
2021-09-18 13:11:11 +0200

    Updated tornado version of profile/merge-profile dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jansuran03/lein-tornado "0.1.0"
+(defproject org.clojars.jansuran03/lein-tornado "0.1.1"
   :description "A Clojure plugin for automatic compilation of Tornado stylesheets."
   :url "https://github.com/JanSuran03/lein-tornado"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"

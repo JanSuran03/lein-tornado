f8b255b7b52cd762f1b4c62aef2b7e6951dff550
JanSuran <suran.orgpad@gmail.com>
2021-09-19 21:13:13 +0200

    Updated the version of required Tornado library.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.jansuran03/lein-tornado "0.1.1"
+(defproject org.clojars.jansuran03/lein-tornado "0.1.2"
   :description "A Clojure plugin for automatic compilation of Tornado stylesheets."
   :url "https://github.com/JanSuran03/lein-tornado"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[ns-tracker "0.4.0"]
-                 [org.clojars.jansuran03/tornado "0.1.4"]
+                 [org.clojars.jansuran03/tornado "0.1.5"]
                  [me.raynes/fs "1.4.6"]]
   :eval-in-leiningen true)

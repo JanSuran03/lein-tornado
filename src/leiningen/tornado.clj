f537be0822fe2428c917c82128c1707d56b2240e
JanSuran <suran.orgpad@gmail.com>
2021-09-19 23:22:20 +0200

    Improved error messages: No weird long stacktraces anymore, the error message gets printed out and the process gets calmly aborted instead.

--- src/leiningen/tornado.clj
+++ src/leiningen/tornado.clj
@@ -6,6 +6,13 @@
             [leiningen.core.eval :refer [eval-in-project]]
             [leiningen.help :as help]))
 
+(defn error
+  "Pretty prints an exception (no 40 lines of unreadable stacktrace) -
+  prints out the error message and calmly aborts the process."
+  [& args]
+  (apply lein/info args)
+  (lein/abort))
+
 (defn- all-builds
   "Returns a sequence of Tornado builds."
   [project]
@@ -16,31 +23,24 @@
   build map of the build. Otherwise, throws an exception."
   [project build-ids]
   (let [all-builds (all-builds project)]
-    (for [build-id build-ids
-          :let [build (if-let [build (some (fn [{:keys [id] :as build}]
-                                             (when (= (name build-id) (name id))
-                                               build)) all-builds)]
-                        build
-                        (throw (Exception. (str "Unknown build id: " build-id))))]
-          :when build]
-      build)))
+    (for [build-id build-ids]
+      (if-let [build (some (fn [{:keys [id] :as build}]
+                             (when (= (name build-id) (name id))
+                               build)) all-builds)]
+        build
+        (error "Unknown build id: " build-id)))))
 
 (defn- validate-builds
   "For each build, validates id, stylesheet and source paths. Throws an exception if some
-  of the builds is not valid.."
+  of the builds are not valid."
   [project]
   (doseq [{:keys [id stylesheet source-paths compiler]} (all-builds project)
           :let [{:keys [output-to]} compiler]]
-    (cond (not ((some-fn keyword? string?) id)) (throw
-                                                  (Exception. (str "The build id must be a string or a keyword: " id)))
-          (nil? source-paths) (throw
-                                (Exception. (str "No source paths specified for a build: " (name id))))
-          (nil? stylesheet) (throw
-                              (Exception. (str "No stylesheet specified for a build: " (name id))))
-          (not (symbol? stylesheet)) (throw
-                                       (Exception. (str "Stylesheet value must be a symbol: " stylesheet " in a build: " (name id))))
-          (nil? output-to) (throw
-                             (Exception. (str "No specified output path (:output-to) for a compiled CSS build: " (name id)))))))
+    (cond (not ((some-fn keyword? string?) id)) (error "The build id must be a string or a keyword: " id)
+          (nil? source-paths) (error "No source paths specified for a build: " (name id))
+          (nil? stylesheet) (error "No stylesheet specified for a build: " (name id))
+          (not (symbol? stylesheet)) (error "Stylesheet value must be a symbol: " stylesheet " in a build: " (name id))
+          (nil? output-to) (error "No specified output path (:output-to) for a compiled CSS build: " (name id)))))
 
 (defn- load-namespaces [stylesheets]
   `(require ~@(for [stylesheet stylesheets]
@@ -56,8 +56,7 @@
                           fs/parent)]
     (when-not (fs/exists? output-to-dir)
       (when-not (fs/mkdirs output-to-dir)                   ;; Returns true on success, false on fail
-        (lein/info "Failed creating a directory: " output-to-dir)
-        (lein/abort)))))
+        (error "Failed creating a directory: " output-to-dir)))))
 
 (defn- compile-builds
   "Automatically recompiles modified builds when needed, or once if watch? is not truthy."
